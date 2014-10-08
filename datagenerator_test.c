ef4a3375e2ec5fc693f01c478f4a175a5c0de0a4
withstand <chenqin1980@gmail.com>
2014-10-08 17:41:57 +0100

    Verified data file generation Java/C/Matlabt

--- datagenerator_test.c
+++ datagenerator_test.c
@@ -1,69 +1,75 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "datagenerator.h"
 
-int  main()
-{
-	int m=4;
-	int k;
-	double shift[10];
-	int shuffle[10];
-	double Qout[100];
-	FILE* fid;
-	char filename[256];
-	int i,j;
+int main()
+{    
+    // new variables
+    int i,j,k;
+    char Mfile[256], Sfile[256], Permfile[256];
+    int cf_numbers[] = {1,1,1,1,1,1,1,1,1,1,1,1,5,3,5};
+    int func_num;
+    int dim;
+    double Mdata[4500], Sdata[150];
+    FILE* fid;
+    //double Qout[900];
+    int shuffle[30];
+    int cf_number;
+    
+    cec15_reseed(12345, 67890, 98765, 43210);
+    
+    for (func_num = 1; func_num <= 15; func_num++) {
+        for (dim = 10; dim<=30; dim+= 20){
+            sprintf(Mfile, "M_%d_D%d.txt",func_num,dim);
+            sprintf(Sfile, "shift_data_%d_D%d.txt", func_num, dim);
+            sprintf(Permfile, "shuffle_data_%d_D%d.txt", func_num, dim);
+            cf_number = cf_numbers[func_num-1];
+            
+            //Mdata = (double*)malloc(dim*cf_number*dim*sizeof(double));
+            for (i = 1; i<= cf_number; i++) {
+                cec15_rotate(dim, Mdata+(i-1)*dim);
+            }
+            
+            //Sdata = (double*)malloc(dim*cf_number*sizeof(double));;
+            for (i = 1; i<= cf_number; i++) {
+                cec15_rand_matrix(1, dim, Sdata+(i-1)*dim);
+                for (j=0; j<dim; j++){
+                    *(Sdata+(i-1)*dim+j) *= 160.0;
+                    *(Sdata+(i-1)*dim+j) -= 80.0;
+                }
+            }
+            cec15_shuffle(dim, shuffle);
+            
+            fid = fopen(Permfile,"w");
+            for (k=0; k<dim; k++)
+                fprintf(fid,"%d\t", shuffle[k]);
+            fclose(fid);
+            
+            fid = fopen(Mfile,"w");
+            for (j=0; j<cf_number*dim; j++) {
+                for (k=0; k<dim; k++) {
+                    fprintf(fid,"%24.16e ", Mdata[j*dim + k]);
+                }
+                fprintf(fid,"\n");
 
-	cec15_reseed(10,20,30,40);
-
-	for(i=0; i<10; i++)
-	{
-		cec15_print_state();
-		printf("%g\n", cec15_rand());
-	}
-
-
-	cec15_reseed(10,20,30,40);
-
-	for (k=0; k<10; k++)
-	{
-		cec15_rotate(m, Qout);
-		sprintf(filename,"M4-%d.txt", k+1);
-		fid = fopen(filename, "w");
-		for (i=0; i<m; i++)
-		{
-			for (j=0; j<m; j++)
-			{
-				fprintf(fid, "%15.6e\t", Qout[i*m+j]);
-			}
-			fprintf(fid,"\n");
-		}
-		fclose(fid);
-	}
-
-
-
-	cec15_shift(m, shift);
-	sprintf(filename,"shift-%d.txt",m);
-	fid = fopen(filename, "w");
-	for (i=0; i<m; i++)
-	{
-		fprintf(fid, "%15.6e\t", shift[i]);
-	}
-	fprintf(fid,"\n");
-	fclose(fid);
-
-
-
-
-	cec15_shuffle(m, shuffle);
-
-	sprintf(filename,"shuffle-%d.txt",m);
-	fid = fopen(filename, "w");
-	for (i=0; i<m; i++)
-		fprintf(fid, "%.0f\t", shuffle[i]);
-	fprintf(fid,"\n");
-	fclose(fid);
+            }
+            fclose(fid);
 
 
-	return 0;
+            fid = fopen(Sfile,"w");
+            for (j=0; j<cf_number; j++) {
+                for (k=0; k<dim; k++) {
+                    fprintf(fid,"%24.16e ", Sdata[j*dim + k]);
+                }
+                fprintf(fid,"\n");
+            }
+            fclose(fid);
+            
+            //free(Mdata);
+            //free(Sdata);
+            
+        }
+    }
+    return 0;
 }
--- java/DataGeneratorTest.java
+++ java/DataGeneratorTest.java
@@ -1,5 +1,5 @@
 import org.ieee.cec15.cbcsono.DataGeneratorJNI;
-import java.util.Arrays;
+import java.io.*;
 
 
 public class DataGeneratorTest {
@@ -13,31 +13,108 @@ public class DataGeneratorTest {
    // Declare a native method sayHello() that receives nothing and returns void
    // Test Driver
 public static void main(String[] args) {
- DataGeneratorJNI jk = new DataGeneratorJNI();
 
- jk.reseed(10,20,30,40);
- double [][] mat = jk.rotate(2);
- if (mat!=null)
- {
-  System.out.println(Arrays.toString(mat[0]));
-  System.out.println(Arrays.toString(mat[1]));
+  try{
+
+    DataGeneratorTest.ProduceDataFiles();
+
+  }catch(IOException e)
+  {
+    System.out.println("IO Error " + e);
+  }
+//  DataGeneratorJNI jk = new DataGeneratorJNI();
+
+//  jk.reseed(10,20,30,40);
+//  double [][] mat = jk.rotate(2);
+//  if (mat!=null)
+//  {
+//   System.out.println(Arrays.toString(mat[0]));
+//   System.out.println(Arrays.toString(mat[1]));
+// }
+
+// jk.reseed(10,20,30,40);
+// mat = jk.rand(2,2);
+// if (mat!=null)
+// {
+//   System.out.println(Arrays.toString(mat[0]));
+//   System.out.println(Arrays.toString(mat[1]));
+// }
+// jk.reseed(10,20,30,40);
+
+
+
+// int [] s = jk.shuffle(10);
+// if (s!=null)
+//   System.out.println(Arrays.toString(s));
+
+
 }
 
-jk.reseed(10,20,30,40);
-mat = jk.rand(2,2);
-if (mat!=null)
-{
-  System.out.println(Arrays.toString(mat[0]));
-  System.out.println(Arrays.toString(mat[1]));
+
+public static void ProduceDataFiles()   throws IOException
+{    
+    // new variables
+  int cf_numbers[] = {1,1,1,1,1,1,1,1,1,1,1,1,5,3,5};
+
+  DataGeneratorJNI jk = new DataGeneratorJNI();
+
+  jk.reseed(12345, 67890, 98765, 43210);
+
+
+  for (int func_num = 1; func_num <= 15; func_num++) {
+    for (int dim = 10; dim<=30; dim+= 20){
+
+
+      int cf_number = cf_numbers[func_num-1];
+
+            //Mdata = (double*)malloc(dim*cf_number*dim*sizeof(double));
+      String Mfile = "M_" + func_num + "_D"+ dim +".txt";
+
+      PrintWriter out = new PrintWriter(Mfile);
+
+      for (int i = 1; i<= cf_number; i++) {
+        double[][] Mdata = jk.rotate(dim);
+        for (int j=0; j<dim; j++) {
+          for (int k=0; k<dim; k++) {
+            out.printf("%24.16e ", Mdata[j][k]);
+          }
+          out.printf("\n");
+        }
+
+      }
+
+      out.close();
+
+
+      String Sfile = "shift_data_" + func_num + "_D"+ dim +".txt";
+
+      out = new PrintWriter(Sfile);
+      for (int i = 1; i<= cf_number; i++) {
+        double [][] Sdata = jk.rand(1, dim);
+        for (int j=0; j<dim; j++) {
+          out.printf("%24.16e ", Sdata[0][j]);
+        }
+        out.printf("\n");
+      }
+      out.close();
+
+
+      int []shuffle = jk.shuffle(dim);
+
+
+      String Permfile = "shuffle_data_" + func_num + "_D" + dim + ".txt";
+
+      out = new PrintWriter(Permfile);
+      for (int k=0; k<dim; k++)
+        out.printf("%d\t", shuffle[k]);
+      out.close();
+
+           //free(Mdata);
+            //free(Sdata);
+
+    }
+  }
 }
-jk.reseed(10,20,30,40);
 
 
-
-int [] s = jk.shuffle(10);
-if (s!=null)
-  System.out.println(Arrays.toString(s));
-
-
-}
 }
