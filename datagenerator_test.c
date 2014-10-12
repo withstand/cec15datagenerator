04803f085ebd19769d1fe3aee5928ba950825527
withstand <chenqin1980@gmail.com>
2014-10-12 14:50:23 +0100

    Set output format to 24.6f instead of 24.6e to make sure the result has exact precision as the program generated.

--- datagenerator_test.c
+++ datagenerator_test.c
@@ -49,7 +49,7 @@ int main()
             fid = fopen(Mfile,"w");
             for (j=0; j<cf_number*dim; j++) {
                 for (k=0; k<dim; k++) {
-                    fprintf(fid,"%24.6e ", Mdata[j*dim + k]);
+                    fprintf(fid,"%24.6f ", Mdata[j*dim + k]);
                 }
                 fprintf(fid,"\n");
 
@@ -60,7 +60,7 @@ int main()
             fid = fopen(Sfile,"w");
             for (j=0; j<cf_number; j++) {
                 for (k=0; k<dim; k++) {
-                    fprintf(fid,"%24.6e ", Sdata[j*dim + k]);
+                    fprintf(fid,"%24.6f ", Sdata[j*dim + k]);
                 }
                 fprintf(fid,"\n");
             }
